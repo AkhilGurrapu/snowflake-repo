48224c49e0d82d8d22970c7f789d8fa5cddba737
AkhilGurrapu <akhilricky97@gmail.com>
2025-08-07 02:55:42 +0000

    snowsarva setup

--- intial_setup.sql
+++ intial_setup.sql
@@ -1,10 +1,32 @@
-create user if not exists akhil;
-alter user akhil set password = 'AKHIL@1997';
-CREATE ROLE tutorial1_role;
+-- Create the snowsarva_role user/role
+create user snowsarva_user;
+CREATE ROLE snowsarva_role;
 
-GRANT ROLE tutorial1_role TO USER akhil;
+-- Grant the snowsarva_role to the user 
+GRANT ROLE snowsarva_role TO USER snowsarva_user;
+ALTER USER snowsarva_user ADD PROGRAMMATIC ACCESS TOKEN test_token;
+ALTER USER snowsarva_user SET NETWORK_POLICY = my_ip;
+
+-- Grant required privileges to the role
+GRANT CREATE INTEGRATION ON ACCOUNT TO ROLE snowsarva_role;
+GRANT CREATE WAREHOUSE ON ACCOUNT TO ROLE snowsarva_role;
+GRANT CREATE DATABASE ON ACCOUNT TO ROLE snowsarva_role;
+GRANT CREATE APPLICATION PACKAGE ON ACCOUNT TO ROLE snowsarva_role;
+GRANT CREATE APPLICATION ON ACCOUNT TO ROLE snowsarva_role;
+GRANT CREATE COMPUTE POOL ON ACCOUNT TO ROLE snowsarva_role WITH GRANT OPTION;
+GRANT BIND SERVICE ENDPOINT ON ACCOUNT TO ROLE snowsarva_role WITH GRANT OPTION;
 
 
-GRANT ALL PRIVILEGES ON warehouse compute_wh TO ROLE tutorial1_role;
-GRANT CREATE APPLICATION PACKAGE ON ACCOUNT TO ROLE tutorial1_role;
-GRANT CREATE APPLICATION ON ACCOUNT TO ROLE tutorial1_role;
+-- Set the current context to use the snowsarva_role
+USE ROLE snowsarva_role;
+-- Create or replace the warehouse for the app
+CREATE OR REPLACE WAREHOUSE snowsarva_warehouse
+  WITH WAREHOUSE_SIZE = 'X-SMALL'
+  AUTO_SUSPEND = 180
+  AUTO_RESUME = true
+  INITIALLY_SUSPENDED = false;
+
+-- Create the image repository used by the app
+CREATE DATABASE snowsarva_image_database;
+CREATE SCHEMA snowsarva_image_schema;
+CREATE IMAGE REPOSITORY snowsarva_image_repo;
